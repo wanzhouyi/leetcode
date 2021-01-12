514562d92587a67edde789a1836d4e7d2edb1b66
mango <wanzhouyi@qq.com>
2021-01-13 00:17:22 +0800

    199. 二叉树的右视图

---
+++ 6.树/二叉树/199. 二叉树的右视图.py
@@ -0,0 +1,44 @@
+"""
+给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+示例:
+输入:[1,2,3,null,5,null,4]
+输出:[1, 3, 4]
+解释:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-right-side-view
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        result = []
+        if not root:
+            return []
+        stack = [root]
+        while stack:
+            temp_stack = stack.copy()
+            stack.clear()
+            result.append(temp_stack[-1].val)
+            for node in temp_stack:
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+        return result
