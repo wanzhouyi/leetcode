de7c34bfc33429eabe1949f50f61c5892cd6d35c
mango <wanzhouyi@qq.com>
2021-01-13 00:15:55 +0800

    102. 二叉树的层序遍历

---
+++ 6.树/二叉树/102. 二叉树的层序遍历.py
@@ -0,0 +1,53 @@
+"""
+给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+
+示例：
+二叉树：[3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回其层序遍历结果：
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        from collections import deque
+        result = []
+        if not root:
+            return []
+        stack = deque([root])
+        while stack:
+            temp_tack = stack.copy()
+            stack.clear()
+            temp_ans = []
+            while temp_tack:
+                node = temp_tack.popleft()
+                temp_ans.append(node.val)
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+            result.append(temp_ans)
+        return result
