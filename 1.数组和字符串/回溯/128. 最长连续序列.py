34fd5df4dd18147558813370789331e57460c417
mango <wanzhouyi@qq.com>
2021-01-15 23:43:27 +0800

    332. 重新安排行程

---
+++ 1.数组和字符串/回溯/128. 最长连续序列.py
@@ -0,0 +1,55 @@
+"""
+给定一个机票的字符串二维数组 [from, to]，子数组中的两个成员分别表示飞机出发和降落的机场地点，对该行程进行重新规划排序。所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 开始。
+提示：
+
+如果存在多种有效的行程，请你按字符自然排序返回最小的行程组合。例如，行程 ["JFK", "LGA"] 与 ["JFK", "LGB"] 相比就更小，排序更靠前
+所有的机场都用三个大写字母表示（机场代码）。
+假定所有机票至少存在一种合理的行程。
+所有的机票必须都用一次 且 只能用一次。
+
+示例 1：
+
+输入：[["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+输出：["JFK", "MUC", "LHR", "SFO", "SJC"]
+示例 2：
+
+输入：[["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+输出：["JFK","ATL","JFK","SFO","ATL","SFO"]
+解释：另一种有效的行程是["JFK","SFO","ATL","JFK","ATL","SFO"]。但是它自然排序更大更靠后。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reconstruct-itinerary
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from typing import List
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        if not tickets:
+            return []
+        pathes = []
+
+        def back_track(tickts: list, start, path):
+            if pathes:
+                return
+            if not tickets:
+                pathes.append(path.copy())
+                return []
+            ls = [x for x in tickts if x[0] == start]
+            if not ls:
+                return
+            ls.sort(key=lambda x: x[1])
+            for ju in ls:
+                path.append(ju[1])
+                tickts.remove(ju)
+                back_track(tickts, ju[1], path)
+                tickets.append(ju)
+                path.pop()
+
+        back_track(tickets, "JFK", path=["JFK"])
+        # print('============================',pathes)
+        if not pathes:
+            return []
+        else:
+            return pathes[0]
