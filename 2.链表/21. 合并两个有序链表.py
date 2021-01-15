b68608430396fbd55d8d78921ca372b035493eb2
mango <wanzhouyi@qq.com>
2021-01-15 23:49:50 +0800

    21.合并两个有序链表

---
+++ 2.链表/21. 合并两个有序链表.py
@@ -0,0 +1,59 @@
+"""
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+示例 1：
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from .ListNodeHelper import ListNode
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        pre = ListNode(0)
+        current = pre
+        while l1 and l2:
+            if l1.val > l2.val:
+                current.next = l2
+                l2 = l2.next
+            else:
+                current.next = l1
+                l1 = l1.next
+            current = current.next
+        if l1:
+            current.next = l1
+        if l2:
+            current.next = l2
+        return pre.next
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        elif not l2:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
