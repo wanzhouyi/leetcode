4249d626c6ab27c0d9b83b94303bfe3d7746b49a
mango <wanzhouyi@qq.com>
2021-02-19 23:36:50 +0800

    49

---
+++ 1.数组和字符串/字符串/字母异位词/49. 字母异位词分组.py
@@ -0,0 +1,46 @@
+"""
+给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+
+示例:
+
+输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+输出:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+说明：
+
+所有输入均为小写字母。
+不考虑答案输出的顺序。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/group-anagrams
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import collections
+from typing import List
+
+
+class Solution:
+    # 方法一，字典法，61%
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        n = len(strs)
+        dic = collections.defaultdict(list)
+        for i in range(n):
+            dic[''.join(sorted(strs[i]))].append(strs[i])
+        return list(dic.values())
+
+    # 官解，计数法
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = collections.defaultdict(list)
+
+        for st in strs:
+            counts = [0] * 26
+            for ch in st:
+                counts[ord(ch) - ord("a")] += 1
+            # 需要将 list 转换成 tuple 才能进行哈希
+            mp[tuple(counts)].append(st)
+
+        return list(mp.values())
