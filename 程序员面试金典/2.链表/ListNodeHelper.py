39d51911cd985d3d06d3a17614bac5778b9d5cdb
mango <wanzhouyi@qq.com>
2021-05-01 23:34:46 +0800

    02.01. 移除重复节点

---
+++ 程序员面试金典/2.链表/ListNodeHelper.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def create_listnode(nums: list):
+    dummy = ListNode(None)
+    current = dummy
+    for _, num in enumerate(nums):
+        current.next = ListNode(num)
+        current = current.next
+    return dummy.next
+
+
+def listnode_to_list(head: ListNode):
+    result = []
+    while head:
+        result.append(head.val)
+        head = head.next
+    return result
