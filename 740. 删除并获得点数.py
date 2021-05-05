e62cd8d46651997d884c5cb75d3dbeddf29be850
mango <wanzhouyi@qq.com>
2021-05-05 23:25:02 +0800

    740. 删除并获得点数

---
+++ 740. 删除并获得点数.py
@@ -0,0 +1,70 @@
+"""
+给你一个整数数组nums，你可以对它进行一些操作。
+每次操作中，选择任意一个nums[i]，删除它并获得nums[i]的点数。之后，你必须删除每个等于nums[i] - 1或nums[i] + 1的元素。
+开始你拥有 0 个点数。返回你能通过这些操作获得的最大点数。
+
+示例 1：
+输入：nums = [3,4,2]
+输出：6
+解释：
+删除 4 获得 4 个点数，因此 3 也被删除。
+之后，删除 2 获得 2 个点数。总共获得 6 个点数。
+
+示例2：
+输入：nums = [2,2,3,3,3,4]
+输出：9
+解释：
+删除 3 获得 3 个点数，接着要删除两个 2 和 4 。
+之后，再次删除 3 获得 3 个点数，再次删除 3 获得 3 个点数。
+总共获得 9 个点数。
+
+提示：
+
+1 <= nums.length <= 2 * 104
+1 <= nums[i] <= 104
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/delete-and-earn
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+import collections
+from typing import List
+
+
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        maxVal = max(nums)
+        total = [0] * (maxVal + 1)
+        for val in nums:
+            total[val] += val
+
+        def rob(nums: List[int]) -> int:
+            size = len(nums)
+            first, second = nums[0], max(nums[0], nums[1])
+            for i in range(2, size):
+                first, second = second, max(first + nums[i], second)
+            return second
+
+        return rob(total)
+
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        maxVal = max(nums)
+        total = [0] * (maxVal + 1)
+        for val in nums:
+            total[val] += val
+
+        from functools import lru_cache
+        @lru_cache
+        def rob2(totals):
+            print(totals)
+            if not totals:
+                return 0
+            ans = max(totals[0] + rob2(totals[2:]), rob2(totals[1:]))
+            return ans
+
+        return rob2(tuple(total))
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.deleteAndEarn([2, 2, 3, 3, 3, 4]))
