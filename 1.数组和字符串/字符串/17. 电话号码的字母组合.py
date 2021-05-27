1652179208c7c7528ab0e893eb8159f94af9bf3c
mango <wanzhouyi@qq.com>
2021-05-27 22:33:21 +0800

    17.电话号码的字母组合

---
+++ 1.数组和字符串/字符串/17. 电话号码的字母组合.py
@@ -0,0 +1,101 @@
+"""
+给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+
+
+ 
+
+示例 1：
+
+输入：digits = "23"
+输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+示例 2：
+
+输入：digits = ""
+输出：[]
+示例 3：
+
+输入：digits = "2"
+输出：["a","b","c"]
+ 
+
+提示：
+
+0 <= digits.length <= 4
+digits[i] 是范围 ['2', '9'] 的一个数字。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno',
+               '7': 'pqrs',
+               '8': 'tuv', '9': 'wxyz'}
+
+        n = len(digits)
+        idx = 0
+        ans = [""]
+        while idx < n:
+            chars = dic[digits[idx]]
+            temp_ans = []
+            for char in chars:
+                for a in ans:
+                    temp_ans.append(a + char)
+            ans = temp_ans
+            idx += 1
+        return ans if ans != [''] else []
+
+
+# 以下是官解
+
+class Solution:
+    """
+    方法一：回溯
+    首先使用哈希表存储每个数字对应的所有可能的字母，然后进行回溯操作。
+
+    回溯过程中维护一个字符串，表示已有的字母排列（如果未遍历完电话号码的所有数字，则已有的字母排列是不完整的）。该字符串初始为空。每次取电话号码的一位数字，从哈希表中获得该数字对应的所有可能的字母，并将其中的一个字母插入到已有的字母排列后面，然后继续处理电话号码的后一位数字，直到处理完电话号码中的所有数字，即得到一个完整的字母排列。然后进行回退操作，遍历其余的字母排列。
+
+    回溯算法用于寻找所有的可行解，如果发现一个解不可行，则会舍弃不可行的解。在这道题中，由于每个数字对应的每个字母都可能进入字母组合，因此不存在不可行的解，直接穷举所有的解即可。
+
+    作者：LeetCode-Solution
+    链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number/solution/dian-hua-hao-ma-de-zi-mu-zu-he-by-leetcode-solutio/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    """
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return list()
+
+        phoneMap = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(index: int):
+            if index == len(digits):
+                combinations.append("".join(combination))
+            else:
+                digit = digits[index]
+                for letter in phoneMap[digit]:
+                    combination.append(letter)
+                    backtrack(index + 1)
+                    combination.pop()
+
+        combination = list()
+        combinations = list()
+        backtrack(0)
+        return combinations
