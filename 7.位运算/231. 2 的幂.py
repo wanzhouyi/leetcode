69feae03b114dbe763ea3e14b46ed9261fbbb493
mango <wanzhouyi@qq.com>
2021-05-30 01:13:26 +0800

    231.2的幂

--- 7.位运算/231. 2 的幂.py
+++ 7.位运算/231. 2 的幂.py
@@ -80,6 +80,9 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         return n > 0 and (n & (n - 1)) == 0
 
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and (n & -n) == n
 
 if __name__ == '__main__':
     s = Solution()
