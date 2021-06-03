d709fdb0b9fff1c2a401e04f7b79121c7589f715
mango <wanzhouyi@qq.com>
2021-06-04 01:07:25 +0800

    160.

--- 2.链表/160. 相交链表.py
+++ 2.链表/160. 相交链表.py
@@ -49,3 +49,41 @@ class Solution:
             headB = headB.next
 
         return None
+
+
+class Solution:
+    """
+    设「第一个公共节点」为 node ，「链表 headA」的节点数量为 a ，「链表 headB」的节点数量为 b ，
+    「两链表的公共尾部」的节点数量为 c ，则有：
+
+    头节点 headA 到 node 前，共有 a - c 个节点；
+    头节点 headB 到 node 前，共有 b - c 个节点；
+
+
+    考虑构建两个节点指针 A , B 分别指向两链表头节点 headA , headB ，做如下操作：
+
+    指针 A 先遍历完链表 headA ，再开始遍历链表 headB ，当走到 node 时，共走步数为：
+    a + (b - c)
+
+    指针 B 先遍历完链表 headB ，再开始遍历链表 headA ，当走到 node 时，共走步数为：
+    b + (a - c)
+
+    如下式所示，此时指针 A , B 重合，并有两种情况：
+
+    a + (b - c) = b + (a - c)
+
+    若两链表 有 公共尾部 (即 c > 0c>0 ) ：指针 A , B 同时指向「第一个公共节点」node 。
+    若两链表 无 公共尾部 (即 c = 0c=0 ) ：指针 A , B 同时指向 nullnull 。
+
+    作者：jyd
+    链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists/solution/intersection-of-two-linked-lists-shuang-zhi-zhen-l/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    """
+
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        A, B = headA, headB
+        while A != B:
+            A = A.next if A else headB
+            B = B.next if B else headA
+        return A
