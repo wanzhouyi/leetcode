1507d952502e35cfda0a38b12841bae2494d92ba
mango <wanzhouyi@qq.com>
2021-06-11 23:45:56 +0800

    15.三数之和

--- 1.数组和字符串/多指针/15. 三数之和.py
+++ 1.数组和字符串/多指针/15. 三数之和.py
@@ -47,3 +47,57 @@ class Solution:
                 elif sm < 0:
                     left += 1
         return [list(x) for x in result]
+
+
+class Solution:
+    """
+    排序 + 双指针
+    本题的难点在于如何去除重复解。
+
+    算法流程：
+    特判，对于数组长度 nn，如果数组为 null 或者数组长度小于 3，返回 []。
+    对数组进行排序。
+    遍历排序后数组：
+    若 nums[i]>0：因为已经排序好，所以后面不可能有三个数加和等于 0，直接返回结果。
+    对于重复元素：跳过，避免出现重复解
+    令左指针 L=i+1，右指针 R=n−1，当 L<R 时，执行循环：
+    当 nums[i]+nums[L]+nums[R]==0，执行循环，判断左界和右界是否和下一位置重复，去除重复解。
+    并同时将 L,R移到下一位置，寻找新的解
+    若和大于 0，说明 nums[R] 太大，R 左移
+    若和小于 0，说明 nums[L] 太小，L 右移
+
+    作者：wu_yan_zu
+    链接：https://leetcode-cn.com/problems/3sum/solution/pai-xu-shuang-zhi-zhen-zhu-xing-jie-shi-python3-by/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    """
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        n = len(nums)
+        res = []
+        if (not nums or n < 3):
+            return []
+        nums.sort()
+        res = []
+        for i in range(n):
+            if (nums[i] > 0):
+                return res
+            if (i > 0 and nums[i] == nums[i - 1]):
+                continue
+            L = i + 1
+            R = n - 1
+            while (L < R):
+                if (nums[i] + nums[L] + nums[R] == 0):
+                    res.append([nums[i], nums[L], nums[R]])
+                    while (L < R and nums[L] == nums[L + 1]):
+                        L = L + 1
+                    while (L < R and nums[R] == nums[R - 1]):
+                        R = R - 1
+                    L = L + 1
+                    R = R - 1
+                elif (nums[i] + nums[L] + nums[R] > 0):
+                    R = R - 1
+                else:
+                    L = L + 1
+        return res
