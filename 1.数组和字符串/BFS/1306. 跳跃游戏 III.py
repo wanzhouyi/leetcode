fb76aa80ee676307284778af989421a76905ecfd
mango <wanzhouyi@qq.com>
2021-07-11 23:50:55 +0800

    451. 根据字符出现频率排序

--- 1.数组和字符串/BFS/1306. 跳跃游戏 III.py
+++ 1.数组和字符串/BFS/1306. 跳跃游戏 III.py
@@ -64,7 +64,9 @@ class Solution:
     方法一：广度优先搜索
     我们可以使用广度优先搜索的方法得到从 start 开始能够到达的所有位置，如果其中某个位置对应的元素值为 0，那么就返回 True。
 
-    具体地，我们初始时将 start 加入队列。在每一次的搜索过程中，我们取出队首的节点 u，它可以到达的位置为 u + arr[u] 和 u - arr[u]。如果某个位置落在数组的下标范围 [0, len(arr)) 内，并且没有被搜索过，则将该位置加入队尾。只要我们搜索到一个对应元素值为 0 的位置，我们就返回 True。在搜索结束后，如果仍然没有找到符合要求的位置，我们就返回 False。
+    具体地，我们初始时将 start 加入队列。在每一次的搜索过程中，我们取出队首的节点 u，它可以到达的位置为 u + arr[u] 和 u - arr[u]。
+    如果某个位置落在数组的下标范围 [0, len(arr)) 内，并且没有被搜索过，则将该位置加入队尾。
+    只要我们搜索到一个对应元素值为 0 的位置，我们就返回 True。在搜索结束后，如果仍然没有找到符合要求的位置，我们就返回 False。
 
     作者：LeetCode-Solution
     链接：https://leetcode-cn.com/problems/jump-game-iii/solution/tiao-yue-you-xi-iii-by-leetcode-solution/
